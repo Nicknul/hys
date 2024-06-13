12324d216846cff2eb04c1038e74a05d14776133
정호연 <jea7730@gmail.com>
2024-06-13 11:07:18 +0900

    POST 데이터 받기 확인 완료

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
  * *2024.06.12
  * * 서버 열기_완료
  * *2024.06.13
- * * GET 요청이면 main.html 생성 _ 진행 중
+ * ? GET 요청이면 main.html 생성_완료
  * * POST, /submit 요청 받기 _ 진행 중
  */
 
 const http = require('http');
+const qs = require('node:querystring');
+
 const fileSystem = require('./module/fileSystem.js');
 const path = require('./module/path.js');
 const string = require('./module/string.js');
@@ -15,6 +17,15 @@ const server = http.createServer((req, res) => {
   req.url = decodeURI(req.url);
   console.log('유효성 검사:', req.url);
   if (req.method === 'POST' && req.url === '/submit') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      let data = qs.parse(body);
+      let title = data.title;
+      let content = data.content;
+    });
   }
   if (req.method === 'GET' && req.url === '/') {
     fileSystem.write(path.none('main'), string.main('파일 목록이 들어갈 자리'));
