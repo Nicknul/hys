1c61b18668809895947fe74ef10cf9a42520e6db
정호연 <jea7730@gmail.com>
2024-06-13 11:25:53 +0900

    빈 배열에 list 값 push

--- app.js
+++ app.js
@@ -9,33 +9,43 @@ const string = require('./module/string.js');
  * ? GET 요청이면 main.html 생성_완료
  * ? POST, /submit 요청 받기_완료
  * ? /submit이면 list 폴더 안에 파일 생성_완료
- * * list 폴더 안에 있는 파일 목록 읽어오기_진행 중
+ * ? list 폴더 안에 있는 파일 목록 읽어오기_완료
  */
 
 const http = require('http');
 const qs = require('node:querystring');
 
 const server = http.createServer((req, res) => {
+  // 한글로 된 주소의 암호를 풀어줌
   req.url = decodeURI(req.url);
   console.log('유효성 검사:', req.url);
+
+  let listArr = [];
+
+  // 요청 메서드가 POST이고, 주소 뒤에가 /submit이면 실행
   if (req.method === 'POST' && req.url === '/submit') {
     let body = '';
-
+    // 데이터 받아오기
     req.on('data', (chunk) => {
       body += chunk.toString();
     });
-
+    // 데이터 받아오기가 끝나면 실행
     req.on('end', () => {
       let data = qs.parse(body);
       let title = data.title;
       let content = data.content;
-
+      // 받아온 데이터를 토대로 파일 생성
       fileSystem.write(path.list(title), string.create(title, content, '댓글이 들어갈 자리'));
-
-      //* list 폴더 안에 있는 파일 목록 읽어옴
+      // list 폴더 안에 있는 파일 목록 읽어옴
       let list = fileSystem.dir('./list');
+
+      for (let fileName in list) {
+        listArr.push(list[fileName]);
+      }
+      console.log(listArr);
     });
   }
+  // 요청 메서드가 GET이고 주소 뒤에가 /이라면 실행
   if (req.method === 'GET' && req.url === '/') {
     fileSystem.write(path.none('main'), string.main('파일 목록이 들어갈 자리'));
     let data = fileSystem.read(path.none('main'));
