67710db149e65499500c3243e755a45beed198dc
정호연 <jea7730@gmail.com>
2024-06-13 11:12:23 +0900

    파일 생성 완료

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
+const fileSystem = require('./module/fileSystem.js');
+const path = require('./module/path.js');
+const string = require('./module/string.js');
+
 /**
  * *2024.06.12
  * ? 서버 열기_완료
  * *2024.06.13
  * ? GET 요청이면 main.html 생성_완료
  * ? POST, /submit 요청 받기_완료
+ * * /submit이면 list 폴더 안에 파일 생성_진행 중
  */
 
 const http = require('http');
 const qs = require('node:querystring');
-
-const fileSystem = require('./module/fileSystem.js');
-const path = require('./module/path.js');
-const string = require('./module/string.js');
 
 const server = http.createServer((req, res) => {
   req.url = decodeURI(req.url);
   console.log('유효성 검사:', req.url);
   if (req.method === 'POST' && req.url === '/submit') {
     let body = '';
+
     req.on('data', (chunk) => {
       body += chunk.toString();
     });
+
     req.on('end', () => {
       let data = qs.parse(body);
       let title = data.title;
       let content = data.content;
+
+      fileSystem.write(path.list(title), string.create(title, content, '댓글이 들어갈 자리'));
     });
   }
   if (req.method === 'GET' && req.url === '/') {
